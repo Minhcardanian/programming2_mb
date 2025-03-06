6fcb4ca563b1afe46d9d1ae45259798f36321277
Minh Bui <90388903+Minhcardanian@users.noreply.github.com>
2025-03-06 10:34:34 +0700

    Create convert.cpp

---
+++ convert.cpp
@@ -0,0 +1,41 @@
+/* C Version
+#include <stdio.h>
+
+int sumArray(int arr[], int size) {
+    int sum = 0;
+    for (int i = 0; i < size; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Sum of array elements: %d\n", sumArray(arr, size));
+
+    return 0;
+*/
+}
+
+// C++ Version
+#include <iostream>
+using namespace std;
+
+int sumArray(int arr[], int size) {
+    int sum = 0;
+    for (int i = 0; i < size; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Sum of array elements: " << sumArray(arr, size) << endl;
+    
+    return 0;
+}
